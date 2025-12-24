a9440d8adda2016a36daf77cd9a8a588ca4722c5
i.rajasekaran <iniyarajasekaran2004@gmail.com>
2025-12-24 13:37:13 +0530

    Prime Number

---
+++ Prime_Number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Prime_Number {
+    public static void main(String[] args){
+
+        System.out.print("Enter the number:");
+        Scanner num= new Scanner(System.in);
+        int Num= num.nextInt();
+        System.out.println("The prime no till "+Num+":" );
+        for(int i=1;i<=Num;i++){
+            int count=0;
+            for(int j=1;j<=i;j++){
+                if(i%j==0){
+                    count=count+1;
+                }
+            }
+            if(count==2){
+                System.out.println(i);
+            }
+
+        }
+
+
+    }
+
+}
